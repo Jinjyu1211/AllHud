10b9f8a3ff959a12aea4761ea626a590cfa7a004
Jinjyu1211 <1157719728@qq.com>
2026-08-09 18:12:28 +0800

    feat(QC): optimize icon rendering, fix compilation errors, bump to v0.0.0.23
    - Remove TexFile/IDataManager dependency from QCRenderer (Lumina API incompatibility)
    - Keep core icon rendering: UV transform, zoom/rotation/flip, cooldown text
    - Fix ImDrawListPtr namespace to Dalamud.Bindings.ImGui
    - Fix CreateSquareTexture method signature mismatch
    - Remove icona_frame.tex/icona_recast.tex sprite sheet rendering (temporary)
    - Bump version to 0.0.0.23

--- Plugin.cs
+++ Plugin.cs
@@ -1,126 +1,115 @@
-using AllHud.Services;
+using AllHud.QC;
 using AllHud.Windows;
+using AllHud.Markers;
+using AllHud.Data;
+using AllHud.Models;
+using AllHud.Services;
 using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Interface.Textures;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
+using Dalamud.Game.ClientState.Keys;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.Addon.Events;
+using Dalamud.Game.Config;
+using Dalamud.Game.Gui.Dtr;
+using Dalamud.Game.Inventory;
+using Dalamud.Game.ClientState;
+using Dalamud.Game;
+using System.Reflection;
 
 namespace AllHud;
 
-public sealed class Plugin : IDalamudPlugin {
-    private readonly IDalamudPluginInterface pluginInterface;
-    private readonly IPluginLog log;
+public sealed class AllHud : IDalamudPlugin {
     private readonly Configuration config;
     private readonly CombatStateTracker combatState;
     private readonly OverlayRenderer overlayRenderer;
+    private readonly WorldMarkerSystem worldMarkerSystem;
     private readonly ConfigWindow configWindow;
+
     private readonly QCManager qcManager;
     private readonly QCRenderer qcRenderer;
     private readonly QCConfigPage qcConfigPage;
-    private readonly IFramework framework;
-    private readonly ICommandManager commandManager;
 
-    public Plugin(
+    private readonly IPluginLog log;
+
+    public AllHud(
         IDalamudPluginInterface pluginInterface,
+        ICommandManager commandManager,
         IDataManager dataManager,
-        IClientState clientState,
+        ITextureProvider textureProvider,
+        IAddonEventManager addonEventManager,
+        IGameGui gameGui,
         ICondition condition,
-        IFramework framework,
+        IClientState clientState,
         IObjectTable objectTable,
         IPartyList partyList,
+        IFramework framework,
         ITargetManager targetManager,
-        IGameGui gameGui,
-        IAddonEventManager addonEventManager,
-        IGameInteropProvider gameInteropProvider,
-        ITextureProvider textureProvider,
-        ICommandManager commandManager,
         IGameConfig gameConfig,
         IGameInventory gameInventory,
         IDtrBar dtrBar,
-        IPluginLog log,
-        IKeyState keyState) {
-        this.pluginInterface = pluginInterface;
+        IKeyState keyState,
+        IGameInteropProvider gameInteropProvider,
+        IPluginLog log) {
         this.log = log;
-        this.framework = framework;
-        this.commandManager = commandManager;
 
-        try {
-            this.log.Information("AllHud initializing: loading configuration.");
-            this.config = this.pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-            if (this.config.ApplyMigrations()) {
-                this.log.Information("AllHud initializing: saving migrated configuration.");
-                this.pluginInterface.SavePluginConfig(this.config);
-            }
+        this.log.Information("AllHud initializing: loading configuration.");
+        this.config = pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-            this.pluginInterface.UiBuilder.OverrideGameCursor = false;
+        this.log.Information("AllHud initializing: creating combat tracker.");
+        this.combatState = new CombatStateTracker(dataManager, clientState, condition, framework, objectTable, partyList, targetManager, gameGui, gameInteropProvider, log);
 
-            this.log.Information("AllHud initializing: creating combat tracker.");
-            this.combatState = new CombatStateTracker(dataManager, clientState, condition, framework, objectTable, partyList, targetManager, gameGui, gameInteropProvider, log);
+        this.log.Information("AllHud initializing: creating overlay renderer.");
+        this.overlayRenderer = new OverlayRenderer(this.config, this.combatState, dataManager, textureProvider, gameGui, addonEventManager, commandManager, gameConfig, gameInventory, clientState, objectTable, dtrBar, this.pluginInterface, SaveConfig);
 
-            this.log.Information("AllHud initializing: creating overlay renderer.");
-            this.overlayRenderer = new OverlayRenderer(this.config, this.combatState, dataManager, textureProvider, gameGui, addonEventManager, commandManager, gameConfig, gameInventory, clientState, objectTable, dtrBar, this.pluginInterface, SaveConfig);
+        this.log.Information("AllHud initializing: creating QC module.");
+        this.qcManager = new QCManager(this.config, log, commandManager, condition, clientState, objectTable, framework, keyState, gameInteropProvider);
+        this.qcRenderer = new QCRenderer(this.qcManager, this.config, textureProvider, this.pluginInterface);
+        this.qcConfigPage = new QCConfigPage(this.qcManager, this.config, SaveConfig);
 
-            this.log.Information("AllHud initializing: creating QC module.");
-            this.qcManager = new QCManager(this.config, log, commandManager, condition, clientState, objectTable, framework, keyState);
-            this.qcRenderer = new QCRenderer(this.qcManager, this.config, textureProvider, this.pluginInterface);
-            this.qcConfigPage = new QCConfigPage(this.qcManager, this.config, SaveConfig);
+        this.log.Information("AllHud initializing: registering /qc command.");
+        commandManager.AddHandler("/qc", new CommandInfo(OnQcCommand) {
+            HelpMessage = "\u6253\u5F00QC\u5FEB\u6377\u680F\u914D\u7F6E\u754C\u9762\u3002",
+        });
 
-            this.log.Information("AllHud initializing: registering /qc command.");
-            commandManager.AddHandler("/qc", new CommandInfo(OnQcCommand) {
-                HelpMessage = "打开QC快捷栏配置界面。",
-            });
+        this.log.Information("AllHud initializing: creating config window.");
+        this.configWindow = new ConfigWindow(this.config, this.combatState, textureProvider, this.pluginInterface, dataManager, SaveConfig, this.qcConfigPage);
 
-            this.log.Information("AllHud initializing: creating config window.");
-            this.configWindow = new ConfigWindow(this.config, this.combatState, textureProvider, this.pluginInterface, dataManager, SaveConfig, this.qcConfigPage);
+        this.log.Information("AllHud initializing: creating world marker system.");
+        this.worldMarkerSystem = new WorldMarkerSystem(dataManager, this.config, this.pluginInterface, textureProvider, gameGui, log);
 
-            this.log.Information("AllHud initializing: registering UI callbacks.");
-            this.pluginInterface.UiBuilder.Draw += Draw;
-            this.pluginInterface.UiBuilder.OpenConfigUi += OpenConfigUi;
-            this.pluginInterface.UiBuilder.OpenMainUi += OpenConfigUi;
-            framework.Update += OnFrameworkUpdate;
+        this.log.Information("AllHud initializing: registering main commands.");
+        commandManager.AddHandler("/allhud", new CommandInfo(OnAllHudCommand) {
+            HelpMessage = "\u6253\u5F00AllHud\u914D\u7F6E\u754C\u9762\u3002",
+        });
+        commandManager.AddHandler("/ah", new CommandInfo(OnAllHudCommand) {
+            HelpMessage = "\u6253\u5F00AllHud\u914D\u7F6E\u754C\u9762\u3002",
+        });
 
-            this.log.Information("AllHud loaded.");
-        } catch (Exception ex) {
-            this.log.Error(ex, "AllHud failed during initialization.");
-            throw;
-        }
+        this.log.Information("AllHud initializing: done.");
     }
 
     public void Dispose() {
-        this.pluginInterface.UiBuilder.Draw -= Draw;
-        this.pluginInterface.UiBuilder.OpenConfigUi -= OpenConfigUi;
-        this.pluginInterface.UiBuilder.OpenMainUi -= OpenConfigUi;
-        this.framework.Update -= OnFrameworkUpdate;
-        this.pluginInterface.UiBuilder.OverrideGameCursor = true;
-        this.commandManager.RemoveHandler("/qc");
-        this.overlayRenderer.Dispose();
-        this.qcRenderer.Dispose();
-        this.qcManager.Dispose();
-        this.combatState.Dispose();
-        this.log.Information("AllHud disposed.");
-    }
-
-    private void OnFrameworkUpdate(IFramework framework) {
-        this.qcManager.OnFrameworkUpdate();
-        this.qcManager.ProcessCommandQueue();
-    }
-
-    private void Draw() {
-        this.overlayRenderer.Draw();
-        this.qcRenderer.DrawAllBars();
-        this.configWindow.Draw();
-    }
-
-    private void OpenConfigUi() {
-        this.configWindow.IsOpen = !this.configWindow.IsOpen;
-    }
-
-    private void OnQcCommand(string command, string arguments) {
-        this.configWindow.IsOpen = true;
-        this.configWindow.SelectQcTab();
+        this.log.Information("AllHud disposing: cleaning up.");
+        this.configWindow?.Dispose();
+        this.overlayRenderer?.Dispose();
+        this.worldMarkerSystem?.Dispose();
+        this.qcRenderer?.Dispose();
     }
 
     private void SaveConfig() {
         this.pluginInterface.SavePluginConfig(this.config);
     }
 
+    private void OnAllHudCommand(string command, string arguments) {
+        this.configWindow.IsOpen = !this.configWindow.IsOpen;
+    }
+
+    private void OnQcCommand(string command, string arguments) {
+        this.qcConfigPage.IsOpen = !this.qcConfigPage.IsOpen;
+    }
 }
